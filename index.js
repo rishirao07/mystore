706a5c0bdb49f71797c3f8421b314029a8d36150
rishirao07 <rishirao2310@gmail.com>
2025-02-14 14:23:35 +0530

    add

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const products = [
-    {id: 1,name: "P1",price: 34},
-    {id: 2,name: "P2",price: 50},
-    {id: 3,name: "P3",price: 40}
-];
+// const products = [
+//     {id: 1,name: "P1",price: 34},
+//     {id: 2,name: "P2",price: 50},
+//     {id: 3,name: "P3",price: 40}
+// ];
+let products = []
+
+fetch("products.json")
+  .then((response) => response.json()) 
+  .then((data) => (showProducts(data)))
+  
+  
 const cart = {};
 const addToCart = (id) => {
 
@@ -77,13 +84,21 @@ const showCart = () => {
     divCart.innerHTML = str;
 };
 
-const showProducts = () => {
-    let str = "";
+const showProducts = (data) => {
+    products = data
+    let str = "<div class='row'>";
     products.map((value) => {
-    str += `<div id="product">  ${value.id} - ${value.name} - ${value.price}-<button onclick='addToCart(${value.id})'>Add</button></div>`
-});
-   divProducts.innerHTML = str; 
-};
-
+      str += `
+      <div class='box'>
+      <img src='${value.url}'>
+      <h3>${value.name}</h3>
+      <p>${value.desc}</p>
+      <h4>$${value.price}</h4>
+      <button onclick='addToCart(${value.id})'>Add to Cart</button>
+      </div>
+      `;
+    });
+    divProducts.innerHTML = str+"</div>";
+  };
 
 
